dda4ccbe4374beb71bfe0779048496b08c5e34ec
miguelfgp <64861903+miguelfgp@users.noreply.github.com>
2020-05-05 20:45:50 +0200

    Actualizado mensaje UsarPruebaGit.java

--- src/EjercicioGit/UsarPruebaGit.java
+++ src/EjercicioGit/UsarPruebaGit.java
@@ -16,7 +16,7 @@ public class UsarPruebaGit {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo desde Git");
+        System.out.println("Hola mundo desde Github");
         
         PruebaGit prueba = new PruebaGit();
         
