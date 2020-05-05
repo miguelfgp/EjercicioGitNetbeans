362b0898025913b05ef107e14636cad4c005d17f
Miguel FGP <miguel_fgp@hotmail.es>
2020-05-05 20:30:25 +0200

    Añadida clase UsarPruebaGit

---
+++ src/EjercicioGit/UsarPruebaGit.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjercicioGit;
+
+/**
+ *
+ * @author Koto
+ */
+public class UsarPruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo desde Git");
+        
+        PruebaGit prueba = new PruebaGit();
+        
+        prueba.numero = 20;
+        
+        System.out.println(prueba.numero);
+    }
+
+    
+}
